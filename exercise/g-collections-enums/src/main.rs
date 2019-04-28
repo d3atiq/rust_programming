c1d81ef271a569c48db643b7d6c902671f105951
Douglas Atique <douglas.atique@d3atiq.com>
2019-04-28 09:45:26 -0300

    Exercise g

--- exercise/g-collections-enums/src/main.rs
+++ exercise/g-collections-enums/src/main.rs
@@ -5,6 +5,11 @@
 // - Hit, containing the distance from the center (an f64)
 // - Miss
 
+enum Shot {
+    Bullseye,
+    Hit(f64),
+    Miss,
+}
 
 // We are going to add a method to this enum!
 impl Shot {
@@ -14,7 +19,11 @@ impl Shot {
         // - 2 points for Hit(x) where x < 3.0
         // - 1 point for Hit(x) where x >= 3.0
         // - 0 points for Miss
-
+        match self {
+            Shot::Bullseye => 5,
+            Shot::Hit(x) => if x < 3.0 { 2 } else { 1 },
+            Shot::Miss => 0,
+        }
     }
 }
 
@@ -31,9 +40,26 @@ fn main() {
     //      - Between 1.0 and 5.0 -- Hit(value)
     //      - Greater than 5.0 -- Miss
 
+    for coord in arrow_coords {
+        coord.print_description();
+        let d : f64 = coord.distance_from_center();
+	shots.push(
+            if d < 1.0 {
+                Shot::Bullseye
+	    } else if d < 5.0 {
+                Shot::Hit(d)
+            } else {
+                Shot::Miss
+            }
+        );
+    }
+
 
     let mut total = 0;
     // 4. Finally, loop through each shot in shots and add its points to total
+    for s in shots {
+        total += s.points();
+    }
 
     println!("Final point total is: {}", total);
 }
