368694e49753c03d0707d4ed1bffd158057d8aaf
Douglas Atique <douglas.atique@d3atiq.com>
2019-04-28 09:44:16 -0300

    Exercise b

--- exercise/b-functions/src/main.rs
+++ exercise/b-functions/src/main.rs
@@ -7,19 +7,24 @@ fn main() {
     // See if you can fix the problem. It's right around here, somewhere.
     {
         let area = area_of(width, height);
+        println!("Area is {}", area);
     }
-    println!("Area is {}", area);
 
     // 3. Uncomment the line below.  Make it work by creating a "volume"
     //    function that multiplies three arguments together and returns the
     //    result.
     //
-    //println!("Volume is {}", volume(width, height, depth));
+    println!("Volume is {}", volume(width, height, depth));
 }
 
 fn area_of(x: i32, y: i32) -> i32 {
     // 2. Fix this function to compute the area given dimensions x and y
     //
     // Bonus: Idiomatic rust doesn't use "return" at the end of a block, fix it!
-    return 123;
+//    return 123;
+  x * y
+}
+
+fn volume(w: i32, h: i32, d: i32) -> i32 {
+    w * h * d
 }
